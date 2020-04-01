3ed453e4d12517a400c1e44c7fd261e62334e9dd
ProgBelarus <gngoog100@gmail.com>
2020-04-01 16:34:44 -0400

    pre-heroku-install

--- run.py
+++ run.py
@@ -5,30 +5,29 @@ from app.catalog.webscraper.madworldnews_com import add_articles_for_page_to_dat
 from datetime import datetime
 from sqlalchemy import exc
 
-flask_app = create_app('dev')
+flask_app = create_app('prod')
 with flask_app.app_context():
     db.create_all()
-    # try:
-    #     if not User.query.filter_by(user_name='administrator').first():
-    #         User.create_user(user='administrator', email='admin@admin.com', password='jbaijvnakovninciewvun')
-    #         # note: password from harry@potters.com is 'secret'
-    #     admin_usr = User.query.filter_by(user_name='administrator').first()
-    #     admin_usr.roles = [Role.query.filter_by(name='Admin').first(), ]
-    #     db.session.commit()
-    #     add_articles_for_date_to_database(datetime(2019, 8, 30))
-    #     add_articles_for_page_to_database(1)
-    #
-    # except exc.IntegrityError:
-    #     flask_app.run()
+    try:
+        if not User.query.filter_by(user_name='administrator').first():
+            User.create_user(user='administrator', email='admin@admin.com', password='jbaijvnakovninciewvun')
+            # note: password from harry@potters.com is 'secret'
+        admin_usr = User.query.filter_by(user_name='administrator').first()
+        admin_usr.roles = [Role.query.filter_by(name='Admin').first(), ]
+        db.session.commit()
+        add_articles_for_date_to_database(datetime(2019, 8, 30))
+        add_articles_for_page_to_database(1)
+
+    except exc.IntegrityError:
+        flask_app.run()
 
 
-    if not User.query.filter_by(user_name='administrator').first():
-        User.create_user(user='administrator', email='admin@admin.com', password='jbaijvnakovninciewvun')
-        # note: password from harry@potters.com is 'secret'
-    admin_usr = User.query.filter_by(user_name='administrator').first()
-    admin_usr.roles = [Role.query.filter_by(name='Admin').first(), ]
-    db.session.commit()
-    add_articles_for_date_to_database(datetime(2019, 8, 30))
-    add_articles_for_page_to_database(1)
-
-    flask_app.run()
+    # if not User.query.filter_by(user_name='administrator').first():
+    #     User.create_user(user='administrator', email='admin@admin.com', password='jbaijvnakovninciewvun')
+    #     # note: password from harry@potters.com is 'secret'
+    # admin_usr = User.query.filter_by(user_name='administrator').first()
+    # admin_usr.roles = [Role.query.filter_by(name='Admin').first(), ]
+    # db.session.commit()
+    # add_articles_for_date_to_database(datetime(2019, 8, 30))
+    # add_articles_for_page_to_database(1)
+    # flask_app.run()
